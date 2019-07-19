25fa2b1dff9a79641a81ef4b26a333423e00c5cd
John Cant <john@brittancreek.com>
2019-07-19 16:47:46 -0700

    Misc stuff

---
+++ tests/features/machine/software_scalability.feature
@@ -0,0 +1,3 @@
+Feature: Software Scalability
+
+Scalability needs to be core to the architecture. Maybe the same core should be able to run on any device.
--- tests/features/steps/mcu_management_steps.py
+++ tests/features/steps/mcu_management_steps.py
@@ -9,26 +9,25 @@ import pathlib
 
 @given(u'an mcu')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given an mcu')
-
+    pass
 
 @when(u'the mcu is powered up')
 def step_impl(context):
     raise NotImplementedError(u'STEP: When the mcu is powered up')
 
+@then(u'the mcu should boot up to default state')
+def step_impl(context):
+    assert context.bootfile.exists(), "Error: Missing boot.py file."
+    assert context.mainfile.exists(), "Error: Missing main.py file."
+
 @then(u'I can verify a production mcu')
 def step_impl(context):
     raise NotImplementedError(u'STEP: Then I can verify a production mcu')
 
-@given(u'a production mcu')
+@given(u'a booted mcu')
 def step_impl(context):
     context.bootfile = pathlib.Path('features/steps/source/boot.py')
 
 @when(u'power is applied')
 def step_impl(context):
     context.mainfile = pathlib.Path('features/steps/source/main.py')
-
-@then(u'the mcu should boot up to default state')
-def step_impl(context):
-    assert context.bootfile.exists(), "Error: Missing boot.py file."
-    assert context.mainfile.exists(), "Error: Missing main.py file."
