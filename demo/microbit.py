9b8e00410accd3700ae350ac0561b234667df3b4
John Cant <john@brittancreek.com>
2019-09-27 12:16:34 -0700

    Update microbit.py
    Annotation

--- demo/microbit.py
+++ demo/microbit.py
@@ -4,7 +4,7 @@ import neopixel
 np = neopixel.NeoPixel(pin1, 12)
 
 red = 0
-green = 60
+green = 60  # arbitrary value
 blue = 0
 
 np.clear()
@@ -12,23 +12,23 @@ np.clear()
 while True:
 
     if accelerometer.was_gesture('shake') is True:
-        gestures = accelerometer.get_gestures()  # clear the gestures history
+        gestures = accelerometer.get_gestures()  # clear the gestures history so we can detect another
         end_while = False
-        pin0.write_digital(1)
+        pin0.write_digital(1)  # set the 'gesture captured' indicator
 
         while True:
             for pixel_id in range(0, len(np) - 1):
                 np[pixel_id] = (red, green, blue)
                 np.show()
-                sleep(50)
+                sleep(50)  # arbitrary value
                 np.clear()
                 if accelerometer.was_gesture('shake') is True:
-                    gestures = accelerometer.get_gestures()  # clear the gestures history
-                    end_while = True
+                    gestures = accelerometer.get_gestures()  # clear the gestures history again ready for next gesture capture
+                    end_while = True  # set flag to end the main loop
                     break  # end the for loop
 
             if end_while:
-                pin0.write_digital(0)
+                pin0.write_digital(0)  # clear the 'gesture captured' indicator
                 break
 
         sleep(250)
