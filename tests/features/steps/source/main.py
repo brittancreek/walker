5fbcf5e7e5d0ab62320bff6f30cb3cca94b35845
John Cant <john@brittancreek.com>
2019-10-16 11:44:47 -0700

    Update main.py
    Updated with refactored demo code

---
+++ tests/features/steps/source/main.py
@@ -0,0 +1,45 @@
+from micropython import const
+from microbit import *
+import neopixel
+
+
+# config
+r = const(0)
+g = const(64)
+b = const(0)
+px = const(12)
+d = const(50)
+m = const(5)
+
+
+# ring
+np = neopixel.NeoPixel(pin1, px)
+np.clear()
+
+while True:
+
+    if accelerometer.was_gesture('shake') is True:
+        gesture_captured(True)
+
+        end_while = False
+        accelerometer.get_gestures()
+
+        while True:
+            for i in range(0, px - 1):
+                np[i] = (r, g, b)
+                np.show()
+                sleep(d)
+                np.clear()
+                if accelerometer.was_gesture('shake') is True:
+                    accelerometer.get_gestures()
+                    gesture_captured(False)
+                    end_while = True
+                    break
+
+            if end_while:
+                break
+
+        sleep(d * m)
+
+    def gesture_captured(x):
+        pin0.write_digital(x)
